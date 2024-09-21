4d2cbb47c265db9df5adf58213c4562e119b18b5
JasonCrk <emerzonkolki@gmail.com>
2024-09-21 18:08:44 -0500

    feat(infrastructure/adapters/repositories): Workflow repository adapter

---
+++ src/main/java/com/autotasks/workflow/infrastructure/adapters/repositories/WorkflowRepositoryAdapter.java
@@ -0,0 +1,34 @@
+package com.autotasks.workflow.infrastructure.adapters.repositories;
+
+import com.autotasks.workflow.domain.entities.Workflow;
+import com.autotasks.workflow.domain.ports.repositories.WorkflowRepository;
+import com.autotasks.workflow.domain.value_objects.WorkflowId;
+import com.autotasks.workflow.infrastructure.persistence.jpa.mappers.WorkflowMapper;
+import com.autotasks.workflow.infrastructure.persistence.jpa.models.JpaWorkflow;
+import com.autotasks.workflow.infrastructure.persistence.jpa.repositories.JpaWorkflowRepository;
+
+import com.autotasks.workflow.shared.domain.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@Transactional("autotasks_workflow_service-transaction_manager")
+public class WorkflowRepositoryAdapter implements WorkflowRepository {
+
+    @Autowired
+    private JpaWorkflowRepository repository;
+
+    @Override
+    public Workflow save(Workflow workflow) {
+        JpaWorkflow workflowSaved = this.repository.save(WorkflowMapper.createModel(workflow));
+        return WorkflowMapper.modelToEntity(workflowSaved);
+    }
+
+    @Override
+    public void deleteById(WorkflowId id) {
+        this.repository.deleteById(UUID.fromString(id.value()));
+    }
+}
