333348e6d04bb1080811afdbda7acbe4aa4673f3
JasonCrk <emerzonkolki@gmail.com>
2024-09-21 17:32:28 -0500

    feat: Add Service annotation

--- src/main/java/com/autotasks/workflow/WorkflowApplication.java
+++ src/main/java/com/autotasks/workflow/WorkflowApplication.java
@@ -1,9 +1,17 @@
 package com.autotasks.workflow;
 
+import com.autotasks.workflow.shared.domain.Service;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+		includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Service.class),
+		value = { "com.autotasks.workflow" }
+)
 public class WorkflowApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/autotasks/workflow/shared/domain/Service.java
@@ -0,0 +1,9 @@
+package com.autotasks.workflow.shared.domain;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+public @interface Service {
+}
