c2cf7adffec9efc8e7eefeac46e41fbf039f4166
JasonCrk <emerzonkolki@gmail.com>
2024-09-22 15:39:56 -0500

    feat(WorkflowRepository.java): Find by id method

--- src/main/java/com/autotasks/workflow/domain/ports/repositories/WorkflowRepository.java
+++ src/main/java/com/autotasks/workflow/domain/ports/repositories/WorkflowRepository.java
@@ -3,7 +3,10 @@ package com.autotasks.workflow.domain.ports.repositories;
 import com.autotasks.workflow.domain.entities.Workflow;
 import com.autotasks.workflow.domain.value_objects.WorkflowId;
 
+import java.util.Optional;
+
 public interface WorkflowRepository {
     Workflow save(Workflow workflow);
     void deleteById(WorkflowId id);
+    Optional<Workflow> findById(WorkflowId id);
 }
