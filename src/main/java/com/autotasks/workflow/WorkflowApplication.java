6ea93163dfcd043e5d499fc7d6d40c7f8fa8daca
JasonCrk <emerzonkolki@gmail.com>
2024-09-22 22:07:06 -0500

    feat(WorkflowApplication.java): Enable OpenFeign

--- src/main/java/com/autotasks/workflow/WorkflowApplication.java
+++ src/main/java/com/autotasks/workflow/WorkflowApplication.java
@@ -4,10 +4,12 @@ import com.autotasks.workflow.shared.domain.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@EnableFeignClients
 @ComponentScan(
 		includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Service.class),
 		value = { "com.autotasks.workflow" }
